213aab05ae0abfacc8671a772126d69275900e0e
Sectioned <Sectioned@users.noreply.github.com>
2018-02-01 11:43:22 +0000

    ok

--- ProjectTests/app/src/main/java/com/example/bengr/project1test/MainActivity.java
+++ ProjectTests/app/src/main/java/com/example/bengr/project1test/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.bengr.project1test;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
-import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.text.AttributedCharacterIterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,14 +19,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        addAnimation();
+    }
+
+    public void addAnimation(){
         TextHW = findViewById(R.id.TextHW);
 
         translatebu= AnimationUtils.loadAnimation(this, R.anim.animationfile);
         TextHW.setText("TEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEST");
         TextHW.startAnimation(translatebu);
-
     }
 
     public void endAnimation(View pView){
@@ -40,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
             TextHW.clearAnimation();
             count++;
         }
-
     }
 
 }
