cf087585bf1f3f693d68484235f8c26a55a57dfd
Sectioned <Sectioned@users.noreply.github.com>
2018-02-15 21:19:37 +0000

    nevermind

--- Hangman/app/src/main/java/com/example/bengr/hangman/MainActivity.java
+++ Hangman/app/src/main/java/com/example/bengr/hangman/MainActivity.java
@@ -8,8 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
