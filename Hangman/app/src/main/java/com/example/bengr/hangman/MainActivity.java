1449b04c2b1514b2a4c6a5e3cf2f2e5b6b4bdf18
Sectioned <Sectioned@users.noreply.github.com>
2018-02-16 11:49:19 +0000

    ok

--- Hangman/app/src/main/java/com/example/bengr/hangman/MainActivity.java
+++ Hangman/app/src/main/java/com/example/bengr/hangman/MainActivity.java
@@ -14,15 +14,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private String wordToGuess;
-    private List<Character> worngLetters = new ArrayList<>();
-    private Random rand = new Random();
-    private ArrayList<String> dictionary;
+    private List<Character> wrongLetters = new ArrayList<>();
     private int wordLength; //Set elsewhere
     private char[] guessedLetter;
     private Button enterTextButton;
@@ -33,22 +30,32 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Initialize the instance and load the main activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Have the word be 5 characters long
         wordLength = 5;
+        //Initialise the guessed letter array with length wordLength
         guessedLetter = new char[wordLength];
+        //Loop through guessedLetter and set each element to _
         for(int i = 0; i < wordLength; i++){
             guessedLetter[i] = '_';
         }
+        //Get a random word
         wordToGuess = getRandomWord();
+        //TODO Remove debug print out word that is chosen
         System.out.println(wordToGuess);
+        //Set the label at the bottom of the screen to be _ of length of word and with spaces
+        //between _'s
         setGuessedLetterLabel();
 
+        //Set the buttons and text view to id.
         enterTextButton = findViewById(R.id.enterGuess);
         guessEntered = findViewById(R.id.inputGuess);
         incorrectLetters = findViewById(R.id.wrongLettersGuessed);
 
+        //Have the button be a listener to call buttonClicked when button pressed
         enterTextButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 buttonClicked();
@@ -57,13 +64,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public String getRandomWord() {
-        createDictionary();
-        return dictionary.get((int)(Math.random() * dictionary.size()));
-    }
-
-    private void createDictionary(){
-        dictionary = new ArrayList<String>();
+    //Function to getRandom word opens text file in assets folder
+    private String getRandomWord(){
+        ArrayList<String> dictionary = new ArrayList<String>();
 
         BufferedReader dict = null; //Holds the dictionary file
         AssetManager am = this.getAssets();
@@ -94,53 +97,82 @@ public class MainActivity extends AppCompatActivity {
         }catch (NullPointerException f){
             f.printStackTrace();
         }
+
+        return dictionary.get((int)(Math.random() * dictionary.size()));
     }
 
+    //Function to set label of correct guessed letter and yet to be guessed letters
+    //as _'s
     private void setGuessedLetterLabel(){
         TextView lettersGuessed = findViewById(R.id.guessedLetters);
         String guessedString = "";
         for(int i = 0; i < wordLength; i++){
+            //If at last letter in word set final letter to not have a character
             if(i == wordLength-1) {
                 guessedString += String.format("%c", guessedLetter[i]);
             }else {
                 guessedString += String.format("%c ", guessedLetter[i]);
             }
         }
+        //Set the text to the guessedString
         lettersGuessed.setText(guessedString);
     }
 
+    //Function when button to enter guess is clicked
     public void buttonClicked(){
+        //Get the string from the textarea and turn it to a lower case string
         String guess = guessEntered.getText().toString().toLowerCase();
+        //Take the first char in word and take that as the guess
         char singleLetterGuess = guess.charAt(0);
+        //Guess function with letter
         guess(singleLetterGuess);
+        //Set the textArea to be empty
         guessEntered.setText("");
 
+        //If out of guesses set content to lose game activity
+        //Else if word has been correctly guessed
         if(wrongGuessesMade == 4){
-            recreate();
+            setContentView(R.layout.activity_losegame);
+            TextView wordReveal = findViewById(R.id.revealTV);
+            Button restartButton = findViewById(R.id.restartButton);
+            //Reveal the word
+            wordReveal.setText(String.format("Unlucky the word was: %s", wordToGuess));
+            //Add onClickListener to change activity to main and recreate the activity.
+            restartButton.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){ setContentView(R.layout.activity_main);recreate();}
+            });
         }else if(wordComplete == wordLength){
             recreate();
         }
     }
 
+    //Function guess pass value guess char.
     public void guess(char guess){
+        //If there is an index of the the letter loop through and set the guessed letters to the letter instead of _
         if(wordToGuess.indexOf(guess) >= 0) {
 			for(int i = 0; i < wordLength; i++) {
 				if(guess == wordToGuess.charAt(i)) {
 					guessedLetter[i] = guess;
+					//Increase word complete
 					wordComplete++;
 				}
 			}
+			//Update the label to include guessed letters
 			setGuessedLetterLabel();
         }else{
-            worngLetters.add(guess);
+            //Add letter to letters not in the word
+            wrongLetters.add(guess);
+            //Update the label with wrong guesses
             wrongLettersUpdate();
+            //Increase wrong guesses made
             wrongGuessesMade++;
         }
     }
 
+    //Update the wrong letters label
     public void wrongLettersUpdate(){
         String theGuesses = "You have guessed: ";
-        for(char i : worngLetters){
+        for(char i : wrongLetters){
             theGuesses += String.format("%c,", i);
         }
         incorrectLetters.setText(theGuesses);
