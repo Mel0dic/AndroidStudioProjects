70a4d29298174d1bbb7db078b8790137ee331b2f
Sectioned <Sectioned@users.noreply.github.com>
2018-04-27 11:32:49 +0100

    ok

--- GameTest/app/src/main/java/com/bgrummitt/gametest/MainActivity.java
+++ GameTest/app/src/main/java/com/bgrummitt/gametest/MainActivity.java
@@ -2,21 +2,27 @@ package com.bgrummitt.gametest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton buttonTemplateTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //TODO remove
-        Log.w("com.bgrummitt.gametest", "Test Warning");
+        buttonTemplateTest = findViewById(R.id.testingButton);
 
-        TextView testButton = findViewById(R.id.mainButton);
-        testButton.setBackgroundResource(R.drawable.button);
+        buttonTemplateTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
