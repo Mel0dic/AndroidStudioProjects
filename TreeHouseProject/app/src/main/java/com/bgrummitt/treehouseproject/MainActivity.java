e23cf3285a51de21731411aa5f26dc423b24e9e2
Sectioned <Sectioned@users.noreply.github.com>
2018-04-28 20:58:07 +0100

    ok

--- TreeHouseProject/app/src/main/java/com/bgrummitt/treehouseproject/MainActivity.java
+++ TreeHouseProject/app/src/main/java/com/bgrummitt/treehouseproject/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView fact;
     private Button newTextBtn;
+    private RelativeLayout mainFactLayout;
+    private int colorToUse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
         newTextBtn = findViewById(R.id.newFactBtn);
         fact = findViewById(R.id.FactText);
+        mainFactLayout = findViewById(R.id.mainActivity);
 
         newTextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fact.setText(Fact.getNewFact());
+                colorToUse = randomColor.getRandomColor();
+                mainFactLayout.setBackgroundColor(colorToUse);
+                newTextBtn.setTextColor(colorToUse);
             }
         });
     }
-
-    public void updateColors(){
-//        (R.layout.activity_main).setBackgroundColor(color);
-    }
-
 }
