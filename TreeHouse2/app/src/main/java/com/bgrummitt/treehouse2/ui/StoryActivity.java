e31c06475a36879cac06b0d1ce8785459052f2c8
Sectioned <Sectioned@users.noreply.github.com>
2018-05-03 22:00:20 +0100

    ok

--- TreeHouse2/app/src/main/java/com/bgrummitt/treehouse2/ui/StoryActivity.java
+++ TreeHouse2/app/src/main/java/com/bgrummitt/treehouse2/ui/StoryActivity.java
@@ -1,27 +1,62 @@
 package com.bgrummitt.treehouse2.ui;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bgrummitt.treehouse2.R;
 import com.bgrummitt.treehouse2.model.Page;
+import com.bgrummitt.treehouse2.model.Story;
 
 public class StoryActivity extends AppCompatActivity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private String name;
+    private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
+        storyImageView = findViewById(R.id.storyImageView);
+        storyTextView = findViewById(R.id.storyText);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
+
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        name = intent.getStringExtra(getString(R.string.key_name));
         if(name == null || name.isEmpty()){
             name = "friend";
         }
         Log.d(TAG, name);
+
+        story = new Story();
+        loadPage(0);
+    }
+
+    private void loadPage(int pageNumber) {
+
+        Page page = story.getPage(pageNumber);
+
+        storyImageView.setImageDrawable(ContextCompat.getDrawable(this, page.getImageId()));
+
+        String pageText = getString(page.getTextId());
+        pageText = String.format(pageText, name);
+        storyTextView.setText(pageText);
+
+        choice1Button.setText(page.getChoice1().getTextId());
+        choice2Button.setText(page.getChoice2().getTextId());
+
     }
 }
