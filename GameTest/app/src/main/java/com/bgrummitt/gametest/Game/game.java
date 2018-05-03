04da61eef50219f5c5eb2a34eae547b5ce39efae
Sectioned <Sectioned@users.noreply.github.com>
2018-05-03 20:09:21 +0100

    ok

---
+++ GameTest/app/src/main/java/com/bgrummitt/gametest/Game/game.java
@@ -0,0 +1,15 @@
+package com.bgrummitt.gametest.Game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bgrummitt.gametest.R;
+
+public class game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- GameTest/app/src/main/java/com/bgrummitt/gametest/UI/MainActivity.java
+++ GameTest/app/src/main/java/com/bgrummitt/gametest/UI/MainActivity.java
@@ -1,12 +1,14 @@
-package com.bgrummitt.gametest;
+package com.bgrummitt.gametest.UI;
 
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.bgrummitt.gametest.Game.game;
+import com.bgrummitt.gametest.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Button Pressed", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, game.class);
+                startActivity(intent);
             }
         });
     }
