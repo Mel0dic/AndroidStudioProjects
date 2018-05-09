393b37af42e0690b989fe0ade8bbcb94c1bb6c09
Sectioned <Sectioned@users.noreply.github.com>
2018-05-09 21:55:19 +0100

    ok

---
+++ Stormy/app/src/main/java/com/bgrummitt/stormy/CurrentWeather.java
@@ -0,0 +1,90 @@
+package com.bgrummitt.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class CurrentWeather {
+
+    private String loacationLabel;
+    private String icon;
+    private long time;
+    private double temperature;
+    private double humidity;
+    private double precipChance;
+    private String summary;
+    private String timeZone;
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public String getLoacationLabel() {
+        return loacationLabel;
+    }
+
+    public void setLoacationLabel(String loacationLabel) {
+        this.loacationLabel = loacationLabel;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getFormatedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+
+        formatter.setTimeZone(TimeZone.getTimeZone(timeZone));
+
+        return formatter.format(new Date((time * 1000)));
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return precipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        this.precipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+}
--- Stormy/app/src/main/java/com/bgrummitt/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/bgrummitt/stormy/MainActivity.java
@@ -2,10 +2,17 @@ package com.bgrummitt.stormy;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -18,6 +25,7 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather currentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,36 +38,73 @@ public class MainActivity extends AppCompatActivity {
         String forecastURL = "https://api.darksky.net/forecast/%s/%f,%f";
         forecastURL = String.format(forecastURL, apiKey, latitude, longitude);
 
-        OkHttpClient client = new OkHttpClient();
+        if(isNetworkAvailable()) {
 
-        Request request = new Request.Builder()
-                .url(forecastURL)
-                .build();
+            OkHttpClient client = new OkHttpClient();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
 
-            }
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                try {
-                    Log.v(TAG, response.body().string());
-                    if(response.isSuccessful()){
-
-                    }else{
-                        alertUserAboutError();
-                    }
-                }catch(IOException e){
-                    Log.d(TAG, "IO Exception caught", e);
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+                            currentWeather = getCurrentDetails(jsonData);
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.d(TAG, "IO Exception caught", e);
+                    } catch (JSONException e){
+                        Log.d(TAG, "JSON Exception caught", e);
+                    }
+                }
+            });
+
+        }else{
+            alertUserAboutError();
+            Toast.makeText(this, R.string.network_unavailable, Toast.LENGTH_LONG).show();
+        }
 
         Log.d(TAG, "Main UI Code Is Running");
 
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
+        JSONObject forcast = new JSONObject(jsonData);
+
+        JSONObject currently = forcast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setLoacationLabel("Alcatraz Island, California");
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(forcast.getString("timezone"));
+
+        Log.d(TAG, currentWeather.getFormatedTime());
+
+        return currentWeather;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return (networkInfo != null) && (networkInfo.isConnected());
     }
 
     private void alertUserAboutError() {
