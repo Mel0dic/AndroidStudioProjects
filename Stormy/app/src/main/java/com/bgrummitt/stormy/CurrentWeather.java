e75edab7838c5c4620da7cfac210d646cf7ae0ed
Sectioned <Sectioned@users.noreply.github.com>
2018-05-10 17:08:27 +0100

    ok

--- Stormy/app/src/main/java/com/bgrummitt/stormy/CurrentWeather.java
+++ Stormy/app/src/main/java/com/bgrummitt/stormy/CurrentWeather.java
@@ -6,14 +6,27 @@ import java.util.TimeZone;
 
 public class CurrentWeather {
 
-    private String loacationLabel;
+    private String locationLabel;
     private String icon;
     private long time;
-    private double temperature;
+    private float temperature;
     private double humidity;
     private double precipChance;
     private String summary;
     private String timeZone;
+
+    public CurrentWeather() {}
+
+    public CurrentWeather(String locationLabel, String icon, long time, float temperature, double humidity, double precipChance, String summary, String timeZone) {
+        this.locationLabel = locationLabel;
+        this.icon = icon;
+        this.time = time;
+        this.temperature = Math.round(temperature * 10) / (float) 10;
+        this.humidity = humidity;
+        this.precipChance = precipChance;
+        this.summary = summary;
+        this.timeZone = timeZone;
+    }
 
     public String getTimeZone() {
         return timeZone;
@@ -23,12 +36,12 @@ public class CurrentWeather {
         this.timeZone = timeZone;
     }
 
-    public String getLoacationLabel() {
-        return loacationLabel;
+    public String getLocationLabel() {
+        return locationLabel;
     }
 
-    public void setLoacationLabel(String loacationLabel) {
-        this.loacationLabel = loacationLabel;
+    public void setLocationLabel(String locationLabel) {
+        this.locationLabel = locationLabel;
     }
 
     public String getIcon() {
@@ -37,6 +50,45 @@ public class CurrentWeather {
 
     public void setIcon(String icon) {
         this.icon = icon;
+    }
+
+    public int getIconID(){
+        //clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+
+        int iconId = R.drawable.clear_day;
+
+        switch (icon){
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+        }
+
+        return iconId;
     }
 
     public long getTime() {
@@ -55,12 +107,12 @@ public class CurrentWeather {
         this.time = time;
     }
 
-    public double getTemperature() {
+    public float getTemperature() {
         return temperature;
     }
 
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
+    public void setTemperature(float temperature) {
+        this.temperature = Math.round(temperature * 10) / (float) 10;
     }
 
     public double getHumidity() {
--- Stormy/app/src/main/java/com/bgrummitt/stormy/MainActivity.java
+++ Stormy/app/src/main/java/com/bgrummitt/stormy/MainActivity.java
@@ -1,20 +1,26 @@
 package com.bgrummitt.stormy;
 
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
-import android.widget.PopupWindow;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bgrummitt.stormy.databinding.ActivityMainBinding;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -27,16 +33,32 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private CurrentWeather currentWeather;
 
+    private ImageView iconImageView;
+    final double latitude = 52.6309;
+    final double longitude = 1.2974;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getForecast(latitude, longitude);
+
+        Log.d(TAG, "Main UI Code Is Running");
+
+    }
+
+    private void getForecast(double latitude, double longitude) {
+        final ActivityMainBinding binding =  DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+
+        TextView darkSky = findViewById(R.id.darkSkyAttribution);
+        darkSky.setMovementMethod(LinkMovementMethod.getInstance());
+
+        iconImageView = findViewById(R.id.iconImageView);
 
         String apiKey = "2e117feca7d4644ad25ba1a2c375d009";
-        double latitude = 37.8267;
-        double longitude = -122.4233;
-        String forecastURL = "https://api.darksky.net/forecast/%s/%f,%f";
-        forecastURL = String.format(forecastURL, apiKey, latitude, longitude);
+        String forecastURL = "https://api.darksky.net/forecast/%s/%f,%f?lang=%s&units=%s";
+        String units = "si";
+        String language = "en";
+        forecastURL = String.format(Locale.UK, forecastURL, apiKey, latitude, longitude, language, units);
 
         if(isNetworkAvailable()) {
 
@@ -49,9 +71,7 @@ public class MainActivity extends AppCompatActivity {
             Call call = client.newCall(request);
             call.enqueue(new Callback() {
                 @Override
-                public void onFailure(Call call, IOException e) {
-
-                }
+                public void onFailure(Call call, IOException e) {}
 
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
@@ -60,6 +80,28 @@ public class MainActivity extends AppCompatActivity {
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
                             currentWeather = getCurrentDetails(jsonData);
+
+                            final CurrentWeather displayWeather = new CurrentWeather(
+                                    currentWeather.getLocationLabel(),
+                                    currentWeather.getIcon(),
+                                    currentWeather.getTime(),
+                                    currentWeather.getTemperature(),
+                                    currentWeather.getHumidity(),
+                                    currentWeather.getPrecipChance(),
+                                    currentWeather.getSummary(),
+                                    currentWeather.getTimeZone()
+                            );
+
+                            binding.setWeather(displayWeather);
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Drawable drawable = getResources().getDrawable(displayWeather.getIconID());
+                                    iconImageView.setImageDrawable(drawable);
+                                }
+                            });
+
                         } else {
                             alertUserAboutError();
                         }
@@ -76,8 +118,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, R.string.network_unavailable, Toast.LENGTH_LONG).show();
         }
 
-        Log.d(TAG, "Main UI Code Is Running");
-
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
@@ -90,10 +130,10 @@ public class MainActivity extends AppCompatActivity {
         currentWeather.setHumidity(currently.getDouble("humidity"));
         currentWeather.setTime(currently.getLong("time"));
         currentWeather.setIcon(currently.getString("icon"));
-        currentWeather.setLoacationLabel("Alcatraz Island, California");
+        currentWeather.setLocationLabel("Norwich, Norfolk");
         currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
         currentWeather.setSummary(currently.getString("summary"));
-        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTemperature(currently.getLong("temperature"));
         currentWeather.setTimeZone(forcast.getString("timezone"));
 
         Log.d(TAG, currentWeather.getFormatedTime());
@@ -108,10 +148,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void alertUserAboutError() {
-
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
+    }
 
+    public void refreshOnClick(View view){
+        getForecast(latitude, longitude);
+        Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
     }
 
 }
