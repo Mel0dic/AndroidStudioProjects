dd22c95b2580c9bfba9e9960e56e3bfae5dbdb68
Sectioned <Sectioned@users.noreply.github.com>
2018-05-18 14:53:08 +0100

    ok

--- GolfCounter/app/src/main/java/com/bgrummitt/golfcounter/MainActivity.java
+++ GolfCounter/app/src/main/java/com/bgrummitt/golfcounter/MainActivity.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends ListActivity {
 
     private static final String PREFS_FILE = "com.bgrummitt.golfcounter.preferences";
-    private static final String KEY_STROKECOUNT = "KEY_STROKECOUNT_";
+    private static final String KEY_STROKECOUNT = "KEY_STROKE_COUNT_";
     private Hole[] holes = new Hole[18];
     private ListAdapter mListAdapter;
     private SharedPreferences mSharedPreferences;
@@ -40,5 +42,25 @@ public class MainActivity extends ListActivity {
         }
         mEditor.apply();
         super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.clearStrokes){
+            mEditor.clear();
+            mEditor.apply();
+            for(Hole hole : holes){
+                hole.setScore(0);
+            }
+            mListAdapter.notifyDataSetChanged();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
